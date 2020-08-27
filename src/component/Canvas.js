70003161a8d45e2fce998cfddd354b251022e8be
Harshit Maurya <hmaurya999@gmail.com>
2020-08-28 04:28:50 +0530

    improve randomizer algo to increase points span

--- src/component/Canvas.js
+++ src/component/Canvas.js
@@ -3,8 +3,7 @@ import React, {useEffect, useRef} from 'react';
 const BLACK = '#000';
 const WHITE = '#FFF';
 
-class Point
-{
+class Point {
     constructor(x, y) {
         this.x = x;
         this.y = y;
@@ -15,10 +14,28 @@ const generateRandomPoint = (limX, limY) => {
     return new Point(Math.random() * limX, Math.random() * limY);
 }
 
+const distance = (p1, p2 = new Point(0, 0)) => {
+    return Math.sqrt(Math.pow(p2.x - p1.x, 2) + Math.pow(p2.y - p1.y, 2));
+}
+
 const generateRandomPoints = (limX, limY, count) => {
+    const diagonal = distance(new Point(limX, limY));
+    const minLineLengthAllowed = diagonal / 4;
+    const maxLineLengthAllowed = diagonal / 2;
     const randomPoints = []
     for (let i = 0; i < count; i++) {
-        randomPoints.push(generateRandomPoint(limX, limY))
+        let randomPoint;
+        while (true) {
+            randomPoint = generateRandomPoint(limX, limY);
+            if (i === 0) {
+                break;
+            }
+            const distanceFromPoint = distance(randomPoint, randomPoints[i - 1])
+            if (distanceFromPoint < maxLineLengthAllowed && distanceFromPoint > minLineLengthAllowed) {
+                break;
+            }
+        }
+        randomPoints.push(randomPoint);
     }
     return randomPoints;
 }
@@ -40,10 +57,10 @@ const Canvas = props => {
     useEffect(() => {
         const canvas = canvasRef.current;
         // Make it visually fill the positioned parent
-        canvas.style.width ='100%';
-        canvas.style.height='100%';
+        canvas.style.width = '100%';
+        canvas.style.height = '100%';
         // ...then set the internal size to match
-        canvas.width  = canvas.offsetWidth;
+        canvas.width = canvas.offsetWidth;
         canvas.height = canvas.offsetHeight;
         const context = canvas.getContext('2d')
         context.fillStyle = BLACK;
